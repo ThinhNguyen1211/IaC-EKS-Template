571392da1ba3b6fbc87c783c79028f8431fa88cb
Thinh Nguyen <nguyentuanthinh1211@gmail.com>
2026-01-31 02:34:40 +0700

    update tf fromt t3.medium to m5a.large

--- Terraform/eks.tf
+++ Terraform/eks.tf
@@ -88,7 +88,7 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      instance_types           = ["t3.medium"]
+      instance_types           = ["m5a.large"]
       force_update_version     = true
       release_version          = var.ami_release_version
       use_name_prefix          = false
